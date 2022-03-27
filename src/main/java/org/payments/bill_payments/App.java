b1f41250a7b48c15be93757ad2012a086af93817
sowjanraj Gottipati <sr>
2022-03-27 17:38:29 +0530

    file with pom

---
+++ src/main/java/org/payments/bill_payments/App.java
@@ -0,0 +1,13 @@
+package org.payments.bill_payments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
